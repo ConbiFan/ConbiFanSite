87972df102902552edb9dd738bd8d6e14e66f08d
Conbi Fan <diver51gence@gmail.com>
2026-07-26 19:26:34 +0900

    Add files via upload

--- site-interactions-config.js
+++ site-interactions-config.js
@@ -1,6 +1,6 @@
 window.CF_INTERACTIONS_CONFIG = {
   supabaseUrl: "https://cfheuamsqgcqekrlmknn.supabase.co",
-  supabaseAnonKey: "sb_publishable_XEFN3JoRDxg08YM3r2BJhg_Rwk6S6eq",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmaGV1YW1zcWdjcWVrcmxta25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MzE0NTksImV4cCI6MjA5MDQwNzQ1OX0.M5k4jEiasGQxqbieKLdDlSXQw0oa3G1AcbhQ7Wnwrk8",
   ownerEmail: "diver51gence@gmail.com",
   siteName: "Conbi Fan",
   ownerRedirectUrl: "https://www.conbifan.f5.si/owner.html"
